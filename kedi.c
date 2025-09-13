7f485bab8affc2b5f7e50adc6e0bf423aeaabe34
Hamsyz <61hamzademirci@gmail.com>
2025-09-13 23:26:56 +0300

    Add files via upload

---
+++ kedi.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+  void miyav(int n);
+
+  int main(void)
+{   int i = 3;
+    while (i > 0)
+   {
+    printf("miyav\n");
+    i--;
+    }
+
+     for(int k = 0; k<=3; k++)
+    {
+        printf("hav\n");
+    }
+
+     miyav(5);
+
+}
+
+
+
+
+
+void miyav(int n)
+{
+    for(int l = 0; l<n; l++)
+    {
+        printf("miyavvvvvv\n");
+
+    }
+}
